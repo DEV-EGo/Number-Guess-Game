3c199b37c687fa9b54e2e51bfa33e1d1faa2ac40
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-07-21 19:56:53 -0400

    update

---
+++ JS/Manifest.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    var ComPick = Math.floor(Math.random() * 4) + 1;
+    var LockTheGame = false;
+
+    // Logging the Computers pick inside the console log incase i have to troubel shoot
+
+    console.log(" Computer pick " + ComPick);
+
+    // creating the "Click" event that receives the users pick
+
+    $(".btn-choice").on("click", function () {
+
+        // this line will prevent the user from changing the option after the game is done
+
+        var
+    })
+})
